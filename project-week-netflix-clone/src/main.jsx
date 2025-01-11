a70966a9690bf5fce482897b2e7b1bf4853ffabd
Inchino <alessandro.incalza22@gmail.com>
2025-01-11 18:15:30 +0100

    Footer

--- project-week-netflix-clone/src/main.jsx
+++ project-week-netflix-clone/src/main.jsx
@@ -3,5 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './index.css';
 import NetflixNavbar from './components/NetflixNavbar';
+import NetflixFooter from './components/NetflixFooter';
 
-createRoot(document.getElementById('root')).render(<NetflixNavbar />)
+createRoot(document.getElementById('root')).render(
+  <>
+  <NetflixNavbar />
+  <NetflixFooter />
+</>
+)
