8bea9a5163bc4af7bca27eea45afd5dcf50e7166
Inchino <alessandro.incalza22@gmail.com>
2025-01-12 18:45:16 +0100

    Extra page

---
+++ extra-page/src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./index.css";
+import NetflixNavbar from "./components/NetflixNavbar";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <NetflixNavbar />
+  </>
+);
