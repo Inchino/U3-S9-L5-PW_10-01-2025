301f059a29890f5ca7cd2139bbf79f13d5ee17bf
Inchino <alessandro.incalza22@gmail.com>
2025-01-12 16:30:11 +0100

    Controllo sulle immagini

--- project-week-netflix-clone/src/components/FilmsList.jsx
+++ project-week-netflix-clone/src/components/FilmsList.jsx
@@ -2,13 +2,14 @@
 import { Row } from "react-bootstrap";
 import SingleImage from "./SingleImage";
 
-const FilmsList = ({ filmsToShow }) => (
+const FilmsList = ({ filmsToShow = [] }) => (
   <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4">
-    {filmsToShow.map((film) => (
+    {filmsToShow.slice(0, 6).map((film) => (
       <SingleImage film={film} key={film.imdbID} />
     ))}
   </Row>
 );
+
 
 export default FilmsList;
 
--- project-week-netflix-clone/src/components/SingleImage.jsx
+++ project-week-netflix-clone/src/components/SingleImage.jsx
@@ -3,7 +3,8 @@ import { Col, Image } from "react-bootstrap";
 
 const SingleImage = ({ film }) => (
   <Col className="mb-2 text-center px-1">
-    <Image src={film.Poster} alt={film.Title} fluid />
+    <Image src={film.Poster} alt={film.Title} style={{ width: "301px", height: "164px" }}
+ fluid />
   </Col>
 );
 
